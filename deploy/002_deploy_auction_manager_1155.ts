2bcea6f405b4c1c5cc85af0dfbb6f030b0a85c61
zian <mynameiszian@gmail.com>
2023-02-03 11:13:04 +0800

    add deployment scripts for manager 1155 and fix price auction 1155 manager

---
+++ deploy/002_deploy_auction_manager_1155.ts
@@ -0,0 +1,25 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import { contractIdentifier, accountIdentifier } from "../identifier";
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const accounts = await hre.getNamedAccounts();
+
+  const deployerAddress = accounts[accountIdentifier.deployer];
+  const baliolaAddress = accounts[accountIdentifier.baliola];
+  const managerAddress = accounts[accountIdentifier.manager];
+
+  const deployer = hre.deployments;
+
+  const contract = contractIdentifier.auctionManager1155;
+
+  await deployer.deploy(contract, {
+    from: deployerAddress,
+    args: [baliolaAddress, managerAddress],
+    log: true,
+    autoMine: true,
+  });
+};
+
+export default func;
+func.tags = [contractIdentifier.auctionManager1155];
