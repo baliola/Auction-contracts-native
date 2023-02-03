774d85b02f5a463ce016a420ecee59e64d0edb20
zian <mynameiszian@gmail.com>
2023-02-03 11:10:15 +0800

    added identifier

---
+++ identifier.ts
@@ -0,0 +1,13 @@
+export const accountIdentifier = {
+  deployer: "deployer",
+  manager: "manager",
+  user_1: "user_1",
+  user_2: "user_2",
+  baliola: "baliola",
+} as const;
+
+export const contractIdentifier = {
+  auctionManager721: "AuctionManager721",
+  auctionManager1155: "AuctionManager1155",
+  fixedPriceAuctionManager1155: "FixedPriceAuctionManager1155",
+} as const;
